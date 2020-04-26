e69daa4efdcae37708d10cbca689d4b7553cf75d
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-04-26 06:41:28 +0530

    Update index.js

--- Node Js/2. Express framework/1. Morgan/index.js
+++ Node Js/2. Express framework/1. Morgan/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 
 const hostname = 'localhost'
 const port = 3000
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next) =>{
 	console.log(req.headers);
