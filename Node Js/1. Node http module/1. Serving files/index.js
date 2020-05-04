e7f932599206cc4f2120dd489cfcd89aaeaa4757
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-05-04 11:51:46 +0530

    REST API end points using express

--- Node Js/1. Node http module/1. Serving files/index.js
+++ Node Js/1. Node http module/1. Serving files/index.js
@@ -40,10 +40,10 @@ const server = http.createServer((req, res) => {
 	}
 	else {
 		res.statusCode = 404;
-			res.setHeader('Content-Type', 'text/html');
-			res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>')
-					
-			return;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>')
+				
+		return;
 	}
 })
 
