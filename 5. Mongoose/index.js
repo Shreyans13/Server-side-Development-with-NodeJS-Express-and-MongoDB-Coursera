4130a4bdd4be3002cb6c86e52e12ebd2a3979361
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-06 05:28:06 +0530

    User Schema Updated

--- 5. Mongoose/index.js
+++ 5. Mongoose/index.js
@@ -9,27 +9,48 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+
+    // newDish.save()
+    
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id,
+            {
+                $set: {description: 'Updated test'}
+            },{
+                new: true
+            })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'i have a feeling!!',
+            author: 'Coursera'
         });
 
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
--- 5. Mongoose/models/dishes.js
+++ 5. Mongoose/models/dishes.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const CommentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +29,8 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [ CommentSchema ]
 },{
     timestamps: true
 });
