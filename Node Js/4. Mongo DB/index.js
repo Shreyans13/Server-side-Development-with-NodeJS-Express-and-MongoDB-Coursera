4762c9ccee1586128b32db4c2b57e3eea4131ea2
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-06 02:59:23 +0530

    Call Back Hell and Promises

--- Node Js/4. Mongo DB/index.js
+++ Node Js/4. Mongo DB/index.js
@@ -5,56 +5,95 @@ const dboper = require("./operations");
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-	
-	assert.equal(err, null);
-	
-	console.log('Connected correctly to server');
+	MongoClient.connect(url).then((client) => {
+		
+		console.log('Connected correctly to server');
 
-	const db = client.db(dbname);
+		const db = client.db(dbname);
 
-	dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes', (result) => {
-		console.log('Insert Document:\n', result.ops);
+		dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes')
+		.then((result) => {
+			console.log('Insert Document:\n', result.ops);
 
-		dboper.findDocument(db, 'dishes', (docs) => {
+			return dboper.findDocument(db, 'dishes')
+		})
+		.then((docs) => {
 			console.log('Found Documents:\n', docs);
 
-			dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
-
-				console.log('Updated Document:\n', result.result)
-
-				dboper.findDocument(db, 'dishes', (docs) => {
-					console.log('Found Documents:\n', docs);
-
-					db.dropCollection('dishes', (result) => {
-						console.log("Droped Collection: ", result);
-
-						client.close();
-					})
-				})
-			})
+			return dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes')
 		})
-	});
+		.then((result) => {
 
-	// const collection = db.collection('dishes');
+			console.log('Updated Document:\n', result.result)
 
-		// collection.insertOne({"name": "Uthappizza", "description": "test"}, (err, result) => {
-		// 	assert.equal(err, null);
+			return dboper.findDocument(db, 'dishes') 
+		})
+		.then((docs) => {
+			console.log('Found Documents:\n', docs);
 
-		// 	console.log('After Insert:\n');
-		// 	console.log(result.ops);
+			return db.dropCollection('dishes')
+		})
+		.then((result) => {
+			console.log("Droped Collection: ", result);
 
-		// 	collection.find({}).toArray((err, docs) => {
-		// 		assert.equal(err, null);
+			client.close();
+		})
+		.catch(err => {console.log(err)});
+	})
+	.catch(err => {console.log(err)});
 
-		// 		console.log('Found:\n');
-		// 		console.log(docs);
 
-		// 		db.dropCollection('dishes', (err, result) => {
-		// 			assert.equal(err, null);
+// Callback hell
+	// MongoClient.connect(url, (err, client) => {
+		
+	// 	assert.equal(err, null);
+		
+	// 	console.log('Connected correctly to server');
 
-		// 			client.close();
-		// 		});
-		// 	});
-		// });
-});
+	// 	const db = client.db(dbname);
+
+	// 	dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes', (result) => {
+	// 		console.log('Insert Document:\n', result.ops);
+
+	// 		dboper.findDocument(db, 'dishes', (docs) => {
+	// 			console.log('Found Documents:\n', docs);
+
+	// 			dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
+
+	// 				console.log('Updated Document:\n', result.result)
+
+	// 				dboper.findDocument(db, 'dishes', (docs) => {
+	// 					console.log('Found Documents:\n', docs);
+
+	// 					db.dropCollection('dishes', (result) => {
+	// 						console.log("Droped Collection: ", result);
+
+	// 						client.close();
+	// 					})
+	// 				})
+	// 			})
+	// 		})
+	// 	});
+	//	// Simple Operations
+	// 		// const collection = db.collection('dishes');
+
+	// 		// collection.insertOne({"name": "Uthappizza", "description": "test"}, (err, result) => {
+	// 		// 	assert.equal(err, null);
+
+	// 		// 	console.log('After Insert:\n');
+	// 		// 	console.log(result.ops);
+
+	// 		// 	collection.find({}).toArray((err, docs) => {
+	// 		// 		assert.equal(err, null);
+
+	// 		// 		console.log('Found:\n');
+	// 		// 		console.log(docs);
+
+	// 		// 		db.dropCollection('dishes', (err, result) => {
+	// 		// 			assert.equal(err, null);
+
+	// 		// 			client.close();
+	// 		// 		});
+	// 		// 	});
+	// 		// });
+	// });
