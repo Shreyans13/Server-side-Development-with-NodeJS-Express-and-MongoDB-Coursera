ad71281884c129c9757f176056d02553adf6d4b1
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-06 01:07:55 +0530

    Mongo DB database

---
+++ Node Js/4. Mongo DB/index.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err, client) => {
+	
+	assert.equal(err, null);
+	
+	console.log('Connected correctly to server');
+
+	const db = client.db(dbname);
+	const collection = db.collection('dishes');
+
+	collection.insertOne({"name": "Uthappizza", "description": "test"}, (err, result) => {
+		assert.equal(err, null);
+
+		console.log('After Insert:\n');
+		console.log(result.ops);
+
+		collection.find({}).toArray((err, docs) => {
+			assert.equal(err, null);
+
+			console.log('Found:\n');
+			console.log(docs);
+
+			db.dropCollection('dishes', (err, result) => {
+				assert.equal(err, null);
+
+				client.close();
+			});
+		});
+	});
+});
