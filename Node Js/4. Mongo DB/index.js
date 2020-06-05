4c0328df044a12df8bd293023dd891c371284d36
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-06 02:14:36 +0530

    Mongo DB operations

--- Node Js/4. Mongo DB/index.js
+++ Node Js/4. Mongo DB/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require("./operations");
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,25 +12,49 @@ MongoClient.connect(url, (err, client) => {
 	console.log('Connected correctly to server');
 
 	const db = client.db(dbname);
-	const collection = db.collection('dishes');
 
-	collection.insertOne({"name": "Uthappizza", "description": "test"}, (err, result) => {
-		assert.equal(err, null);
+	dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes', (result) => {
+		console.log('Insert Document:\n', result.ops);
 
-		console.log('After Insert:\n');
-		console.log(result.ops);
+		dboper.findDocument(db, 'dishes', (docs) => {
+			console.log('Found Documents:\n', docs);
 
-		collection.find({}).toArray((err, docs) => {
-			assert.equal(err, null);
+			dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
 
-			console.log('Found:\n');
-			console.log(docs);
+				console.log('Updated Document:\n', result.result)
 
-			db.dropCollection('dishes', (err, result) => {
-				assert.equal(err, null);
+				dboper.findDocument(db, 'dishes', (docs) => {
+					console.log('Found Documents:\n', docs);
 
-				client.close();
-			});
-		});
+					db.dropCollection('dishes', (result) => {
+						console.log("Droped Collection: ", result);
+
+						client.close();
+					})
+				})
+			})
+		})
 	});
+
+	// const collection = db.collection('dishes');
+
+		// collection.insertOne({"name": "Uthappizza", "description": "test"}, (err, result) => {
+		// 	assert.equal(err, null);
+
+		// 	console.log('After Insert:\n');
+		// 	console.log(result.ops);
+
+		// 	collection.find({}).toArray((err, docs) => {
+		// 		assert.equal(err, null);
+
+		// 		console.log('Found:\n');
+		// 		console.log(docs);
+
+		// 		db.dropCollection('dishes', (err, result) => {
+		// 			assert.equal(err, null);
+
+		// 			client.close();
+		// 		});
+		// 	});
+		// });
 });
