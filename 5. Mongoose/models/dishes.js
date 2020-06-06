68c143f5e4f5500e935694d3dbf429b0c2918553
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-06 05:33:10 +0530

    Security Update

--- 5. Mongoose/models/dishes.js
+++ 5. Mongoose/models/dishes.js
@@ -35,6 +35,6 @@ const dishSchema = new Schema({
     timestamps: true
 });
 
-var Dishes = mongoose.model('Dish', dishSchema);
+let Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
