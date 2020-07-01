68c8cc1609ce87ae6f90881c7864d67b301e80be
Shreyans Jain <shreyans1313@gmail.com>
2020-07-01 18:51:56 +0530

    Delete config.js

--- 14. Passport Facebook/conFusionServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '276369230465897',
-        clientSecret: '5f0f1555e892ae2eb4c659dfdf61fabb'
-    }
-}
